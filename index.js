134ce39a7ab5d80e601f58bce9f7b46b593b7e3c
Maksym Iliev <orioniv30@gmail.com>
2024-11-13 16:29:13 +0300

    10.2

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 const evenNumbers = numbers.filter(function(number) {
     return number % 2 === 0;
 })
-console.log(evenNumbers);
+console.log(evenNumbers)
